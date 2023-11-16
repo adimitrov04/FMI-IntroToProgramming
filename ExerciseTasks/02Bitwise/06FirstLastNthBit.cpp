39bd65b74a5577d26d67c1ca811ba247e9b8ba36
Antoan Dimitrov <antoan.dimitrov04@gmail.com>
2023-11-16 14:06:10 +0200

    Task 06 - Change last bit's position to a constant

--- ExerciseTasks/02Bitwise/06FirstLastNthBit.cpp
+++ ExerciseTasks/02Bitwise/06FirstLastNthBit.cpp
@@ -13,13 +13,14 @@ Output the first, last, and Nth bit of a given unsigned, 32-bit integer.
 int main () {
     int number, n;
     const int posZero = 1; // 2 to the power of 0
+    const int lastPos = 32; // Points to last bit in a 32-bit number
     
     cout << "Enter a number: ";
     cin >> number;
     cout << "Enter a Nth bit position: ";
     cin >> n;
 
-    int LAST_BIT_MASK = posZero << 31;
+    int LAST_BIT_MASK = posZero << (lastPos - 1);
     int NTH_BIT_MASK = posZero << (n - 1);
 
     bool firstBit = (number & 1);
