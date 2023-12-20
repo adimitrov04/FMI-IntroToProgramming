99f86a34c3cade28f815320fdc6514dc8c438e03
Antoan Dimitrov <antoan.dimitrov04@gmail.com>
2023-12-20 19:42:20 +0200

    11 Strings - Initial commit; add Task 01

---
+++ ExerciseTasks/11Strings/01Capitalize.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using std::endl;
+
+typedef char* pStr; // Char array pointer
+
+const int CHAR_LIMIT = 256;
+
+/*
+-------------------- 01 Capitalize --------------------
+Read a string (char array) from console and capitalize
+all letters in it.
+
+---------------------- EXAMPLE: -----------------------
+input:  abCDeef1$
+output: ABCDEEF1$
+-------------------------------------------------------
+*/
+
+/**
+ * Capitalize all lowercase letters in a string.
+ * 
+ * @param src The address of the source.
+ */
+void capitalize (pStr src)
+{
+    if (src == nullptr)
+        return ;
+
+    const int capitalChange = 'A' - 'a';
+    while (*src)
+    {
+        if (*src >= 'a' && *src <= 'z')
+            *src += capitalChange;
+        
+        src++;
+    }
+}
+
+int main ()
+{
+    char str[CHAR_LIMIT] = {0,};
+
+    std::cin.getline(str, CHAR_LIMIT);
+
+    capitalize(str);
+    std::cout << str << endl;
+
+    return 0;
+}
