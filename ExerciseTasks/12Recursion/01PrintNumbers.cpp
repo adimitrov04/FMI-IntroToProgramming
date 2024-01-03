4ec33695efd022c43ed3efd3685bdc5b5372a86d
Antoan Dimitrov <antoan.dimitrov04@gmail.com>
2024-01-03 13:05:19 +0200

    [12Recursion] Add Task 01

---
+++ ExerciseTasks/12Recursion/01PrintNumbers.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstring>
+
+using std::endl;
+
+/*
+--------------------- 01 PrintNumbers ---------------------
+Create a program which takes a number N and prints all
+numbers from N to 1 using a recursive function.
+
+------------------------ EXAMPLE: -------------------------
+input:  5
+output: 1 2 3 4 5
+-----------------------------------------------------------
+*/
+
+void printNumbers (int n)
+{
+    if (n >= 1)
+    {
+        std::cout << n << ' ';
+        printNumbers(n - 1);
+    }
+}
+
+int main ()
+{
+    int n;
+    std::cin >> n;
+
+    printNumbers(n);
+}
